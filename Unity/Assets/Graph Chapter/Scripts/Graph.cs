0bcaaa7da0eceef7e8b8113daaa26936eae107f0
Krtucho <krtucho98@gmail.com>
2022-11-26 20:34:36 -0500

    refactoring and modifying graph script

---
+++ Unity/Assets/Graph Chapter/Scripts/Graph.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Graph : MonoBehaviour {
+
+	[SerializeField]
+	Transform pointPrefab;
+
+	void Awake () {
+		for (int i = 0; i < 10; i++) {
+			Transform point = Instantiate(pointPrefab);
+			point.localPosition = Vector3.right * i;
+			point.localScale = Vector3.one / 5f;
+		}
+	}
+}
